78d362ea434b77b0936713eca7a09fff53673581
asd <asdzog@mail.ru>
2024-04-03 08:12:45 +0400

    add users views and urls

--- habits/views.py
+++ habits/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
+from habits.models import Habit
 from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
 
 
 class HabitCreateAPIView(generics.CreateAPIView):
@@ -12,3 +14,29 @@ class HabitCreateAPIView(generics.CreateAPIView):
         new_habit = serializer.save()
         new_habit.user = self.request.user
         new_habit.save()
+
+
+class PublicHabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [AllowAny,]
+
+    def get_queryset(self):
+        queryset = Habit.objects.all()
+        return queryset.filter(is_public=True)
+
+
+class HabitDetailsAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
