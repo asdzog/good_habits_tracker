4cdf139c3c067dc52f18febe56b34495e84c2330
asd <asdzog@mail.ru>
2024-04-03 04:09:58 +0400

    add user serializer

---
+++ users/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "password"]
+
+    def create(self, validated_data):
+        user = User(email=validated_data["email"])
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
