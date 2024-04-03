324823620bbaa20c1f200d3122cd37115454c56a
asd <asdzog@mail.ru>
2024-04-04 02:39:37 +0400

    add habits to admin panel

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit, PleasantHabit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'action', 'place', 'is_pleasant', 'user')
+    list_filter = ('action', 'user', 'is_pleasant', 'place', )
+
+
+@admin.register(PleasantHabit)
+class PleasantHabitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'action', 'place', 'is_pleasant', 'user')
+    list_filter = ('action', 'user', 'is_pleasant', 'place', )
