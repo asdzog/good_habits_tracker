21700ea8059cc2bdcd4ed8bcfe29deff079e227a
asd <asdzog@mail.ru>
2024-04-03 04:11:41 +0400

    add new settings and requirements

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'django_filters',
+    'rest_framework_simplejwt',
+
     'users',
     'habits',
 ]
