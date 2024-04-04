b4a41eb8503ce6f67dc39626a8c2b5be0355ca0f
asd <asdzog@mail.ru>
2024-04-04 08:43:01 +0400

    add endpoint for private user's habits

--- habits/views.py
+++ habits/views.py
@@ -19,6 +19,16 @@ class PublicHabitListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         queryset = Habit.objects.all()
+        return queryset.filter(user=self.request.user)
+
+
+class PrivateHabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [AllowAny]
+    pagination_class = HabitsPagination
+
+    def get_queryset(self):
+        queryset = Habit.objects.all()
         return queryset.filter(is_public=True)
 
 
