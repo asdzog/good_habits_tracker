a070bf2150b89c0911faa20484195250d3e062f7
asd <asdzog@mail.ru>
2024-04-06 16:23:15 +0400

    add validator for telegram_chat_id field

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from users.models import User
+from users.validators import ChatIDValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email", "password"]
+        fields = ["first_name", "last_name",  "email", "password", "telegram_chat_id"]
+        validators = [ChatIDValidator(chat_id_field='telegram_chat_id'),]
 
     def create(self, validated_data):
         user = User(email=validated_data["email"])
